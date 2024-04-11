ca920ef6f90525a34971640d09ea5293a36daa96
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2024-04-11 14:11:17 +0530

    Add test cases for parsing

--- src/test/java/io/cse/winzigc/TestUtils.java
+++ src/test/java/io/cse/winzigc/TestUtils.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestUtils {
   private static final Path resourceDir = Path.of("src", "test", "resources");
@@ -12,5 +14,16 @@ public class TestUtils {
   public static InputStream readProgramAsStream(String program) throws IOException {
     File file = new File(resourceDir.resolve("winzig_test_programs").resolve(program).toString());
     return new FileInputStream(file);
+  }
+
+  public static ArrayList<String> getOutput(String program) throws IOException {
+    File file =
+        new File(resourceDir.resolve("winzig_test_programs").resolve(program ).toString());
+    Scanner scan = new Scanner(file);
+    ArrayList<String> tree = new ArrayList<>();
+    while (scan.hasNextLine()) {
+      tree.add(scan.nextLine().replace("\n", ""));
+    }
+    return tree;
   }
 }
