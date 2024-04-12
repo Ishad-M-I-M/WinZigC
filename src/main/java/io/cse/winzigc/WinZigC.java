3ac39be894a49ae545a49e0553c828a43b3dc8f2
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2024-04-12 17:38:27 +0530

    Add main function

---
+++ src/main/java/io/cse/winzigc/WinZigC.java
@@ -0,0 +1,20 @@
+package io.cse.winzigc;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class WinZigC {
+  public static void main(String[] args) throws IOException, ParseException {
+    if (args.length != 2 || args[0].compareTo("-ast") != 0) {
+      System.out.println("Invalid input format.");
+      System.exit(1);
+    }
+    File file = new File(args[1]);
+    InputStream input = new FileInputStream(file);
+    ArrayList<String> output = new Parser(Screener.screen((new Scanner(input)).scan())).parse().getTree();
+    for (String line: output) {
+      System.out.println(line);
+    }
+  }
+}
